79f34627382dfa944741970c6542d7b402b86a68
Sanjeev Grampurohit <sanjeev15002@gmail.com>
2021-11-26 01:10:50 -0500

    Delete train.py and rename main.py to train.py

--- train.py
+++ train.py
@@ -1,81 +1,59 @@
-from main import FSDRDataset
-import numpy as np
-import argparse
-import pickle
-import torch
-import os
-import random
-import torch.optim as optim
-import torch.nn as nn
-
-from models import resnet18, Model1
-from torch.utils.data import DataLoader
+from torch.nn.modules.loss import CrossEntropyLoss
+from torch.utils import data
+import matplotlib.pyplot as plt
+from torchvision.transforms import transforms as T
+from torchvision.datasets import ImageFolder
+from argparse import PARSER
 from utils import init_mask
-from torchvision import transforms, datasets
+from torch.utils.data import DataLoader
+import argparse
+from models import ClassificationModel, SegmentationModel
+import torch.optim as optim
+from tqdm import tqdm
+import numpy as np
+from tensorboardX import SummaryWriter
+parser = argparse.ArgumentParser(description='Auto FSDR Training')
 
+parser.add_argument('--batch_size', default=64, type=int)
+parser.add_argument('--num_epochs', default=20, type=int)
+# parser.add_argument()
 
-lr_mask = 1e-3
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--data_path', default='', type=str,
-                    help='image dataset folder')
-parser.add_argument('--num_epochs', default=20, type=int,
-                    help='number of epochs to train the model for')
-parser.add_argument('--batch_size', default=64,
-                    type=int, help='training batch size')
-parser.add_argument('--lr', default=1e-3, type=float, help='learning rate for training')
-# parser.add_argument('--seed', default=42, type=int, help='random seed')
 args = parser.parse_args()
 
-if not os.path.exists('trained_models/'):
-    os.makedirs('trained_models/')
+writer = SummaryWriter()
 
-MODEL_NAME = 'model1'
-MODEL_PATH = 'trained_models/' + MODEL_NAME + '.pt'
+transform = T.Compose([T.ToTensor(), T.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])])
+shape = [3,32,32]
+mask = init_mask(shape=shape)
+train_root = r'C:\Users\sanje\Documents\Projects\mlls_project\datasets\CIFAR10\test' 
+# val_root = ''
 
-random.seed(args.seed)
-torch.manual_seed(args.seed)
-np.random.seed(args.seed)
+train_dataset = ImageFolder(train_root, transform=transform)
+# val_dataset = ImageFolder(val_root)
 
-normalizer = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                  std=[0.229, 0.224, 0.225])
+train_loader = DataLoader(train_dataset, batch_size=args.batch_size)
+# val_loader = DataLoader(val_dataset, batch_size=args.batch_size)
 
-data_transform = transforms.Compose([
-    transforms.Resize((50, 50)),
-    transforms.ToTensor(),
-    normalizer
-])
+# input_shape = [args.batch_size] + shape
+input_shape = shape
+# print(input_shape)
+model = ClassificationModel(input_shape=input_shape, dim=10)
 
-model = Model1()
-train_dataset = ImageDataset(transform=data_transform)
-# train_dataset = FSDRDataset(transform=data_transform)
-train_loader = DataLoader(train_dataset, args.batch_size, shuffle=True)
-cross_entropy = nn.CrossEntropyLoss()
-params = model.parameters()
-
-for i, param in enumerate(params):
-    if i == 0:
-        param.requires_grad = True
-    else:
-        param.requires_grad = True
-
-param.grad = get_mask_grad()
-
-optimizer = optim.Adam(params, lr=args.lr)
-model.train()
+# Training Specifics
+optimizer = optim.Adam(params=model.parameters(), lr=1e-4)
+loss_fn = CrossEntropyLoss()
 
 for epoch in range(args.num_epochs):
     epoch_loss = []
-    for inputs, labels in train_loader:
-        params_optimizer.zero_grad()
-        
+    for i, (inputs, labels) in tqdm(enumerate(train_loader)):
+        optimizer.zero_grad()
+
         outputs = model(inputs)
-        
-        loss = cross_entropy(outputs, labels)
+
+        loss = loss_fn(outputs, labels)
         epoch_loss.append(loss.item())
         loss.backward()
-        params_optimizer.step()
+        optimizer.step()
     avg_loss = np.mean(epoch_loss)
-    print('[%d] loss: %.4f' % (epoch, avg_loss))
-
-    torch.save(model.state_dict(), MODEL_PATH)
+    print('[%d] loss: %0.3f' % (epoch, avg_loss))
+    writer.add_scalar('loss', avg_loss)
